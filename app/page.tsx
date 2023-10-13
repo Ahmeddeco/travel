69c66d65c2b7b49ce1d3f00d3ea4eadeab3a4572
ahmed <hetetlahma@gmail.com>
2023-10-13 19:29:06 +0300

    start1

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <section className=' flex h-screen w-screen'>
-      <h1 className='m-auto'>TRAVEL</h1>
+      <h1 className='m-auto '>TRAVEL</h1>
     </section>
   )
 }
